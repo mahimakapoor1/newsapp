1bd52bebd950eeba4f693c43bc30117617891655
mahimakapoor1 <mahimakapoor165@gmail.com>
2023-06-29 02:07:35 +0530

    Update Newsitem.js

--- project/src/Newsitem.js
+++ project/src/Newsitem.js
@@ -7,7 +7,7 @@ export default function Newsitem(props) {
       <img src={props.image} alt="..." />
       <h2>{props.heading}</h2>
       <p>{props.description}</p>
-      <a href={props.link} target="_blank" >
+      <a href={props.link} target="_blank" rel="noopener noreferrer" >
         Read More
       </a>
     </div>
