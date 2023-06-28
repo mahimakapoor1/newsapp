66d992946dd849300d6a8b3c9ca9307883c63c44
MahimaKapoor1 <mahimakapoor165@gmail.com>
2023-06-29 01:57:38 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useEffect, useState } from 'react'
+import Newsitem from './Newsitem'
+import './App.css'
+export default function App() {
+  const [news,setnews]=useState([])
+  const [search,setsearch]=useState('')
+  const [nowpage,setnowpage] =useState(1)
+  const [totalpage,settotalpage]=useState(1)
+    const getnews=async()=>{
+        var url=await fetch(`https://newsapi.org/v2/everything?q=${search}&from=2023-05-28&sortBy=publishedAt&apiKey=a7469a295f7b403da9683922e37c773c&pageSize=12&page=${nowpage}`)
+        var data=await url.json()
+        setnews(data.articles)
+        if(data.articles)
+        settotalpage(Math.ceil(data.totalResults)/9)
+    }
+    useEffect(()=>{
+      getnews()
+    },[nowpage])
+  const handlesubmit=(e)=>{
+    e.preventDefault();
+    setnowpage(1);
+    getnews()
+  }
+  const handleprev=()=>{
+    if(nowpage>1)
+    setnowpage(nowpage-1)
+  }
+  const handlenext=()=>{
+    if(nowpage<totalpage)
+    setnowpage(nowpage+1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>NEWS APP</h1>
+      <form onSubmit={handlesubmit}>
+        <input  className='search' type='text' placeholder='what to search...' value={search} onChange={(e)=>setsearch(e.target.value)}/>
+        <input type='submit' value="submit" className='button'/>
+      </form>
+      <div className='main'>
+      {news &&(
+        news.map((item,index)=>{
+          return(<div key={index}className='newsitem' >
+            <Newsitem image={item.urlToImage} heading={item.title.slice(0,25)} description={item.description.slice(0,110)} link={item.url} />
+          </div>
+          )
+        })
+      )}
+      </div>
+      <div>
+      <button className="button" onClick={handleprev}>prev</button>
+     <span className="current-page">{nowpage}</span>
+      <button className="button" onClick={handlenext}>next</button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
