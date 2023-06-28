02e95f851cdf9e601088933b2c4200a8d91ac30c
mahimakapoor1 <mahimakapoor165@gmail.com>
2023-06-29 01:41:01 +0530

    Add files via upload

---
+++ project/src/Newsitem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './newsitem.css'
+export default function Newsitem(props) {
+
+  return (
+    <div className='components'>
+      <img src={props.image} alt="..." />
+      <h2>{props.heading}</h2>
+      <p>{props.description}</p>
+      <a href={props.link} target="_blank" >
+        Read More
+      </a>
+    </div>
+  )
+}
